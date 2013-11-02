6fa008e5ea99562ecca65b8108005f28a70a8574
Lunayo <lun@codebadge.com>
2013-11-02 23:23:20 +0000

    nocase - added test cases and implementation for user endpoint

--- SaleBadger/src/main/java/app/saleBadger/UserEndpoint.java
+++ SaleBadger/src/main/java/app/saleBadger/UserEndpoint.java
@@ -1,21 +1,25 @@
 package app.saleBadger;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import app.model.User;
+
 // The users endpoint will be hosted at the URI path "/users"
-@Path("/users")
+@Path("/api/v1/users")
 public class UserEndpoint {
 
     // TODO: update the class to suit your needs
     
     // The Java method will process HTTP GET requests
     @GET 
+    @Path("{username}")
     // The Java method will produce content identified by the MIME Media
-    // type "text/plain"
-    @Produces("text/plain")
-    public String getIt() {
-        return "I can return the users!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser(@PathParam("username") String username) {
+        return null;
     }
 }

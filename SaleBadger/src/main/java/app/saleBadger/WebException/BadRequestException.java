8ca41621417057765143642f33463f2aac7dfa3f
Lunayo <lun@codebadge.com>
2013-11-10 03:19:35 +0000

    added user resource implementation and refactored the class name

---
+++ SaleBadger/src/main/java/app/saleBadger/WebException/BadRequestException.java
@@ -0,0 +1,10 @@
+package app.saleBadger.WebException;
+
+import javax.ws.rs.WebApplicationException;
+
+public class BadRequestException extends WebApplicationException{
+	
+	public BadRequestException() {
+		// TODO Auto-generated constructor stub
+	}
+}
